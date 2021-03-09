b268eeb5b76aca7d9e551524fa5ab8394d9805eb
朱柯颖 <zhukeying@zhukeyingdeMacBook-Pro.local>
2021-03-09 22:48:45 +0800

    initialise

---
+++ Practical5/fibonacci.py
@@ -0,0 +1,14 @@
+# may be i need four variables.three of them are used to make sum.the other one to count the times 
+count=13
+x=1
+y=1
+print(x)
+print(y)
+while count>2:
+  count=count-1
+  z=y
+  y=x+y
+  x=z
+  print(y)  
+
+  
