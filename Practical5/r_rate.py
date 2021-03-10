76f44b1ffd4ab25e11e68d0aa9f8efc09297310b
朱柯颖 <zhukeying@zhukeyingdeMacBook-Pro.local>
2021-03-10 09:04:57 +0800

    initialise

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -1,9 +1,10 @@
 n=84
 r=1.2
-count=0
+count=5
 #x stand for the number of student that have been infected
 x=1
-while count<5:
-  count=count+1
-  x=x*(r**count)
+x=x*(r**count)
+#x is no larger than n
+if x>n:
+  x=n
 print("The r rate is "+str(r)+" and the total number of individuals infected after 5 generations is "+str(x))
