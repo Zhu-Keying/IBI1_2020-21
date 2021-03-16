116df4072354e934af3e72f33a2745a84ad30aa1
Zhu-Keying <79953226+Zhu-Keying@users.noreply.github.com>
2021-03-16 21:05:47 +0800

    Add files via upload

---
+++ List manipulation.py
@@ -0,0 +1,10 @@
+import numpy as np
+gene_lengths=np.array([9410,394141,4442,105338,19149,76779,126550,36296,842,15981])
+exon_counts=([51,1142,42,216,25,650,32533,57,1,523])
+a=np.true_divide(gene_lengths,exon_counts)
+print(sorted(a))
+import matplotlib.pyplot as plt
+n=10
+score=a
+plt.boxplot(score,vert=True,whis=1.5,patch_artist=True)
+plt.show()
