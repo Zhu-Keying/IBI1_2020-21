3a1d3fa26020c1ef1ce16fa1556ece8d691ecd17
Zhu-Keying <79953226+Zhu-Keying@users.noreply.github.com>
2021-03-17 09:22:10 +0800

    Update r_rate.py

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -1,6 +1,8 @@
+#input all of the data
 n=84
 r=1.2
 count=5
 n=n*(r**count)
+#use "str()" to change the type of the data ,then print all the information out
 print("The r rate is "+str(r)+" and the total number of individuals infected after 5 generations is "+str(n))
 
