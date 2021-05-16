f3f543daf44306aa39c313ad03ed7f00b7aefac9
朱柯颖 <zhukeying@zhukeyingdeMacBook-Pro.local>
2021-05-17 03:35:18 +0800

    initialise

---
+++ Practical11/Alignemnt.py
@@ -0,0 +1,82 @@
+file_name1 = input("please input the first fasta file name,for example SOD2_human.fa :")
+file_name2 = input("please input the second fasta file name,for example SOD2_mouse.fa :")
+file_name3 = input("please input the second fasta file name,for example RandomSeq.fa :")
+
+file1=open(file_name1)
+file2=open(file_name2)
+file3=open(file_name3)
+
+seq_human = file1.readlines()
+seq_mouse = file2.readlines()
+seq_random = file3.readlines()
+
+#list the sequences
+for line in seq_human:
+    if not line.startswith('>'):
+        seq1=line.split()[0]
+        
+for line in seq_mouse:
+    if not line.startswith('>'):
+        seq2=line.split()[0]
+
+for line in seq_random:
+    if not line.startswith('>'):
+        seq3=line.split()[0]
+        
+#calcultate the percentage identity 
+def per(seq1,seq2):
+    distance = 0
+    for i in range(len(seq1)):
+        if seq1[i] == seq2[i]:
+            distance += 1
+    per=str(distance/len(seq1)*100)+'%'
+    return per
+
+print("the percentage of identical AA of human and mouse is: " +per(seq1,seq2))
+print("the percentage of identical AA of human and random is: " +per(seq1,seq3))
+print("the percentage of identical AA of mouse and random is: " +per(seq2,seq3))
+
+
+#input the Blosum62 matrix to calculate #BLOSUM62
+#(BLOSUM62 source is adopted from https://www.ncbi.nlm.nih.gov/Class/FieldGuide/BLOSUM62.txt)
+amino = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', 'B', 'Z', 'X', '*']
+blosum = [
+[ 4, -1, -2, -2,  0, -1, -1,  0, -2, -1, -1, -1, -1, -2, -1,  1,  0, -3, -2,  0, -2, -1,  0, -4],
+[-1,  5,  0, -2, -3,  1,  0, -2,  0, -3, -2,  2, -1, -3, -2, -1, -1, -3, -2, -3, -1,  0, -1, -4],
+[-2,  0,  6,  1, -3,  0,  0,  0,  1, -3, -3,  0, -2, -3, -2,  1,  0, -4, -2, -3,  3,  0, -1, -4],
+[-2, -2,  1,  6, -3,  0,  2, -1, -1, -3, -4, -1, -3, -3, -1,  0, -1, -4, -3, -3,  4,  1, -1, -4],
+[ 0, -3, -3, -3,  9, -3, -4, -3, -3, -1, -1, -3, -1, -2, -3, -1, -1, -2, -2, -1, -3, -3, -2, -4],
+[-1,  1,  0,  0, -3,  5,  2, -2,  0, -3, -2,  1,  0, -3, -1,  0, -1, -2, -1, -2,  0,  3, -1, -4],
+[-1,  0,  0,  2, -4,  2,  5, -2,  0, -3, -3,  1, -2, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1, -4],
+[ 0, -2,  0, -1, -3, -2, -2,  6, -2, -4, -4, -2, -3, -3, -2,  0, -2, -2, -3, -3, -1, -2, -1, -4],
+[-2,  0,  1, -1, -3,  0,  0, -2,  8, -3, -3, -1, -2, -1, -2, -1, -2, -2,  2, -3,  0,  0, -1, -4],
+[-1, -3, -3, -3, -1, -3, -3, -4, -3,  4,  2, -3,  1,  0, -3, -2, -1, -3, -1,  3, -3, -3, -1, -4],
+[-1, -2, -3, -4, -1, -2, -3, -4, -3,  2,  4, -2,  2,  0, -3, -2, -1, -2, -1,  1, -4, -3, -1, -4],
+[-1,  2,  0, -1, -3,  1,  1, -2, -1, -3, -2,  5, -1, -3, -1,  0, -1, -3, -2, -2,  0,  1, -1, -4],
+[-1, -1, -2, -3, -1,  0, -2, -3, -2,  1,  2, -1,  5,  0, -2, -1, -1, -1, -1,  1, -3, -1, -1, -4],
+[-2, -3, -3, -3, -2, -3, -3, -3, -1,  0,  0, -3,  0,  6, -4, -2, -2,  1,  3, -1, -3, -3, -1, -4],
+[-1, -2, -2, -1, -3, -1, -1, -2, -2, -3, -3, -1, -2, -4,  7, -1, -1, -4, -3, -2, -2, -1, -2, -4],
+[ 1, -1,  1,  0, -1,  0,  0,  0, -1, -2, -2,  0, -1, -2, -1,  4,  1, -3, -2, -2,  0,  0,  0, -4],
+[ 0, -1,  0, -1, -1, -1, -1, -2, -2, -1, -1, -1, -1, -2, -1,  1,  5, -2, -2,  0, -1, -1,  0, -4],
+[-3, -3, -4, -4, -2, -2, -3, -2, -2, -3, -2, -3, -1,  1, -4, -3, -2, 11,  2, -3, -4, -3, -2, -4],
+[-2, -2, -2, -3, -2, -1, -2, -3,  2, -1, -1, -2, -1,  3, -3, -2, -2,  2,  7, -1, -3, -2, -1, -4],
+[ 0, -3, -3, -3, -1, -2, -2, -3, -3,  3,  1, -2,  1, -1, -2, -2,  0, -3, -1,  4, -3, -2, -1, -4],
+[-2, -1,  3,  4, -3,  0,  1, -1,  0, -3, -4,  0, -3, -3, -2,  0, -1, -4, -3, -3,  4,  1, -1, -4],
+[-1,  0,  0,  1, -3,  3,  4, -2,  0, -3, -3,  1, -1, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1, -4],
+[ 0, -1, -1, -1, -2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -2,  0,  0, -2, -1, -1, -1, -1, -1, -4],
+[-4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4,  1],
+]
+
+# define a function to get the score
+def score(a,b,score):
+	for i in range (len(a)):
+		score=score+blosum[amino.index(a[i])][amino.index(b[i])]
+	return score
+
+print('the score of human and mouse is '+str(score(seq1,seq2,0)))
+print('the score of human and mouse is '+str(score(seq1,seq3,0)))
+print('the score of human and mouse is '+str(score(seq2,seq3,0)))
+
+
+
+
