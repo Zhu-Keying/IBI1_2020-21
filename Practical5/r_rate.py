676cdab86d6852c35b35cfd10b348671a3f04c06
朱柯颖 <zhukeying@zhukeyingdeMacBook-Pro.local>
2021-05-17 01:40:24 +0800

    initialise

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -1,8 +1,15 @@
 #input all of the data
 n=84
 r=1.2
-count=5
-n=n*(r**count)
+# use x to count the rounds
+x=1
+p=False
+while p==False:
+    p=True
+    x=x+1
+    n=n*r+n
+    if x<6:
+        p=False
 #use "str()" to change the type of the data ,then print all the information out
 print("The r rate is "+str(r)+" and the total number of individuals infected after 5 generations is "+str(n))
 
